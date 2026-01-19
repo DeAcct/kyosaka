6bc0150bff06f252bc23ef4b92e508b8dcc4dd04
Hyeon Seong LEE <50110335+DeAcct@users.noreply.github.com>
2026-01-20 03:12:29 +0900

    dev:font optimization

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import "./styles/variable/_color.scss";
 import "./styles/variable/_utils.scss";
-import "./styles/base/_fonts.scss";
 import "./styles/base/_reset.scss";
 import { App } from "@/components/App/App";
 
