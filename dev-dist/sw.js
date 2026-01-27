1bbf4391a50f2ab6ce6042415ac6eba4ba63b63a
Hyeon Seong LEE <50110335+DeAcct@users.noreply.github.com>
2026-01-28 01:52:00 +0900

    fix:fallback height

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b33317ba'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ubc72cmbqjs"
+    "revision": "0.2rs4e1aleo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
