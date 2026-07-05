7c85b1d612cc2e242a8c640d282b739db44278e9
Hyeon Seong Lee <50110335+DeAcct@users.noreply.github.com>
2026-07-05 08:00:56 +0000

    design:improve accent on dayselector

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i5r3tc98omk"
+    "revision": "0.0s3jcb8ujo4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,8 +10,10 @@ export const Header = define("ky-header", { mapping, raw })(
     template() {
       return html`
         <header class="${this.styles.header}">
+        <button class="${this.styles.button}" type="button">
         <ky-icon name="hamburger" class="${this.styles.icon}"></ky-icon>
-          <h1 class="${this.styles.text}">쿄사카</h1>
+        </button>
+        <h1 class="${this.styles.text}">일정</h1>
         </header>
       `;
     }
