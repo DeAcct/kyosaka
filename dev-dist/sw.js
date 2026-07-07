e6832087be557c212252184005b0a4bcc2e8654e
Hyeon Seong LEE <50110335+DeAcct@users.noreply.github.com>
2026-07-08 02:03:17 +0900

    fix: filename

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5u280h25m1c"
+    "revision": "0.bh8o3d45l1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
