8a4ec2372cb70a7533ac1ec2adc8e1ca43d5d68c
Hyeon Seong LEE <50110335+DeAcct@users.noreply.github.com>
2026-07-09 02:07:42 +0900

    design: sticky navrail header

--- src/components/NavRail/NavRail.js
+++ src/components/NavRail/NavRail.js
@@ -77,7 +77,7 @@ export const NavRail = define("nav-rail", { mapping, raw })(
               <ky-icon name="hamburger" class="${this.styles.icon}"></ky-icon>
             </button>
             <strong class="${this.styles.logo}">kyosaka</strong>
-          </head>
+          </header>
           <div class="${this.styles.actions}">
             ${this.actions.map(
               ({ icon, action, text }) => html`
@@ -117,16 +117,14 @@ export const NavRail = define("nav-rail", { mapping, raw })(
             )}
           </ul>
         </nav>
-        ${
-          this.state.mode === "edit"
-            ? html` <plan-editbar
-                @delete="${this.onDelete}"
-                @exit="${this.editor.exitEdit}"
-                :counter="${this.state.deleteSelected.length}"
-                class="${this.styles.editbar}"
-              ></plan-editbar>`
-            : ""
-        }
+        ${this.state.mode === "edit"
+          ? html` <plan-editbar
+              @delete="${this.onDelete}"
+              @exit="${this.editor.exitEdit}"
+              :counter="${this.state.deleteSelected.length}"
+              class="${this.styles.editbar}"
+            ></plan-editbar>`
+          : ""}
         <div class="${this.styles.backdrop}" @click="${this.closeNav}"></div>
       `;
     }
